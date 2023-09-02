b2ca9c2046da3733bbfc400b607d0780ca123999
kkonrad <konrad.e.urban@gmail.com>
2023-09-02 23:54:18 +0200

    api button added but cors error

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,6 +7,8 @@ import { recoverMessageAddress } from "viem";
 import { useSignMessage } from "wagmi";
 import { signIn, signOut, useSession } from "next-auth/react";
 
+import APICallerButton from "./APICallerButton";
+
 export default function Home() {
   const { data: session, status } = useSession();
   const { open, close } = useWeb3Modal();
@@ -22,23 +24,77 @@ export default function Home() {
     console.log(signMessageData);
   }, [signMessageData]);
 
-  return (
+  const API_BASE = "https://repporter.onrender.com";
+
+  const APICallerButton = () => {
+    const ADDRESS = "0x71C9E62FA7293D43765692A408483B2fC7c7f0C6";
+    const SIGNATURE = "yourSignatureForOAuth2Token";
+    const OAUTH2_TOKEN = "yourOAuth2Token";
+    const LINK_TYPE = "github";
+    const LINK_VALUE = "vbuterin";
     
+
+    const addLink = async () => {
+      const response = await fetch(`${API_BASE}/api/addlink`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          address: ADDRESS,
+          signature: SIGNATURE,
+          oauth2Token: OAUTH2_TOKEN,
+          linkType: LINK_TYPE,
+          linkValue: LINK_VALUE,
+        }),
+      });
+
+      const data = await response.json();
+      console.log("Add Link Response:", data);
+    };
+
+    const getLink = async () => {
+      const response = await fetch(
+        `${API_BASE}/api/getlink?address=${ADDRESS}&linkType=${LINK_TYPE}`
+      );
+      const data = await response.json();
+      console.log("Get Link Response:", data);
+    };
+
+    const handleButtonClick = async () => {
+      console.log("API CALLS EXAMPLE");
+      await addLink();
+      await getLink();
+      console.log("End of API Calls");
+    };
+
+    return <button onClick={handleButtonClick}>Call APIs</button>;
+  };
+
+  return (
     <main className="p-12">
       <div className="flex justify-between items-center w-full p-4">
-        <h1 className="text-3xl">Repporter</h1>
+        <div className="wrapper">
+          <div className="typing-demo text-3xl">Repporter</div>
+        </div>
 
         <div className="flex justify-end items-center space-x-12">
-          <a href="https://github.com/0xkkonrad" target="blank" className="text-current">
+          <a
+            href="https://github.com/0xkkonrad"
+            target="blank"
+            className="text-current"
+          >
             <p>Repo</p>
           </a>
         </div>
 
-        
-      </div> 
+      </div>
 
       <div className="flex w-full h-1/2 flex-wrap gap-2">
-        <button className="w-1/3 md:w-1/4 flex-grow bg-[#151515] p-4 md:p-8 text-white text-7xl flex items-center justify-center hover:invert" onClick={() => signIn("github")}>
+        <button
+          className="w-1/3 md:w-1/4 flex-grow bg-[#151515] p-4 md:p-8 text-white text-7xl flex items-center justify-center hover:invert"
+          onClick={() => signIn("github")}
+        >
           Github
         </button>
         <button className="w-1/10 md:w-1/12 flex-grow bg-[#151515] p-4 md:p-8 text-white text-7xl flex items-center justify-center">
@@ -53,11 +109,19 @@ export default function Home() {
         <button className="w-1/10 md:w-1/12 flex-grow bg-[#151515] p-4 md:p-8 text-white text-7xl flex items-center justify-center">
           =
         </button>
-        <button className="w-1/3 md:w-1/4 flex-grow bg-[#151515] p-4 md:p-8 text-white text-7xl flex items-center justify-center hover:invert" onClick={()=> signMessage({
-                message: "DAS"
-              })}>
-            Sign
+        <button
+          className="w-1/3 md:w-1/4 flex-grow bg-[#151515] p-4 md:p-8 text-white text-7xl flex items-center justify-center hover:invert"
+          onClick={() =>
+            signMessage({
+              message: "DAS",
+            })
+          }
+        >
+          Sign
         </button>
+
+        <APICallerButton />
+
       </div>
 
       {/* blobs */}
@@ -66,10 +130,9 @@ export default function Home() {
         <div className="red blob"></div>
         <div className="green blob"></div>
       </div>
-{status === "authenticated" &&
-      <button onClick={() => signOut()}>Sign out</button>
-
-}
+      {status === "authenticated" && (
+        <button onClick={() => signOut()}>Sign out</button>
+      )}
       {status === "authenticated" && <div>{session.user.email}</div>}
       {signMessageData && (
         <div className="bg-white text-black text-xl">
