13082e92e0d217ef36f483334682fcc232552536
igortrsk <igor.tarasiuk4@gmail.com>
2023-09-03 01:18:13 +0200

    api update fuck

--- frontend/types/ next-auth.d.ts
+++ frontend/types/ next-auth.d.ts
@@ -5,6 +5,8 @@ declare module "next-auth" {
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
   interface Session {
+    accessToken : any
+    name:any
     user: {
       /** The user's postal address. */
       address: string
