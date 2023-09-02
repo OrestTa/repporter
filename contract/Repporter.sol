56e871de55448f0854e6d495d8c713f59bf6854f
Orest Tarasiuk <830847+OrestTa@users.noreply.github.com>
2023-09-02 17:41:31 +0200

    contract owner

--- contract/Repporter.sol
+++ contract/Repporter.sol
@@ -1,67 +1,48 @@
 // SPDX-License-Identifier: GPL-3.0
-// from remix.ethereum.org
+// Starting from remix.ethereum.org templates
 
 pragma solidity >=0.7.0 <0.9.0;
+
+import "hardhat/console.sol";
 
 /** 
  * @title Repporter
  * @dev Implements the repporter
  */
 contract Repporter {
-    address public chairperson; // this contract's deployer
+    address public owner;
 
-    uint public quorumInFavour;
-    uint public votesInFavour = 0;
-    uint public votesAgainst = 0;
-    uint public votesAbstaining = 0;
+    // event for EVM logging
+    event OwnerSet(address indexed oldOwner, address indexed newOwner);
 
-    mapping (bytes32 nullifier => bool nullifierExists) public nullifiers;
+    // modifier to check if caller is owner
+    modifier isOwner() {
+        // If the first argument of 'require' evaluates to 'false', execution terminates and all
+        // changes to the state and to Ether balances are reverted.
+        // This used to consume all gas in old EVM versions, but not anymore.
+        // It is often a good idea to use 'require' to check if functions are called correctly.
+        // As a second argument, you can also provide an explanation about what went wrong.
+        require(msg.sender == owner, "Caller is not owner");
+        _;        
+    }
+
+    mapping (address user => bool web2) public links;
 
     /** 
-     * @dev Create a new ballot
-     * @param quorumInFavour_ the minimum number of yes-votes (0) for the ballot to be deemed accepted; 
-     *   can be expanded to support other proposal types (no (1), abstain (2))
+     * @dev Create a Repporter
      */
-    constructor(uint quorumInFavour_) {
-        chairperson = msg.sender;
-        quorumInFavour = quorumInFavour_;
+    constructor() {
+        console.log("Owner contract deployed by:", msg.sender);
+        owner = msg.sender;
+        emit OwnerSet(address(0), owner);
     }
 
     /**
-     * @dev Give your vote
-     * @param proposalType set to 0 to vote for yes, to 1 for no, to 2 for abstain
-     * @param zkProof to verify the vote
-     * @param nullifier to avoid double-voting
+     * @dev Change owner
+     * @param newOwner address of new owner
      */
-    function vote(uint8 proposalType, bytes32 zkProof, bytes32 nullifier) public {
-        if (nullifiers[nullifier]) {
-            revert("Voter has already voted");
-        }
-    
-        bool zkProofIsValid = true; /* TODO: Verify zkProof using generated verifier contract */
-        if (!zkProofIsValid) {
-            revert("Provided zkProof is not valid");
-        }
-
-        nullifiers[nullifier]=true;
-
-        if (proposalType==0) {
-            votesInFavour++;
-        }
-        if (proposalType==1) {
-            votesAgainst++;
-        }
-        if (proposalType==2) {
-            votesAbstaining++;
-        }
+    function changeOwner(address newOwner) public isOwner {
+        emit OwnerSet(owner, newOwner);
+        owner = newOwner;
     }
-
-    /**
-     * @dev Check if the ballot's in-favour votes passed the required in-favour quorum
-     */
-    function ballotWasAccepted() public view 
-        returns (bool accepted) 
-        {
-            return votesInFavour >= quorumInFavour;
-        }
 }
