b51dd89bb3255ca3c665ccab092fb35300181e55
Hugo Montenegro <h@hugomontenegro.com>
2023-09-02 17:59:36 +0200

    backend init

---
+++ backend/app.py
@@ -0,0 +1,8 @@
+# Repporter backend. Main functionality:
+# 1. receive link Request from frontend.
+#   - verify oauth token
+#   - verify address signature
+#   - writes to smart contract
+#   - returns transaction hash
+# 2. Bonus: API to read from smart contract
+
